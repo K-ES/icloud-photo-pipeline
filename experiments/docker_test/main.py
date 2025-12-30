86db8264c18919ad4770b467adaedab302dabd23
Евгений Кривоногов <evgeniy.krivonogov@glowbyteconsulting.com>
2025-12-30 10:28:06 +0200

    создание своего первого образа docker

---
+++ experiments/docker_test/main.py
@@ -0,0 +1,6 @@
+import calendar
+print('Добро пожаловать в супер календарь\n')
+year = int(input('Год: '))
+month = int(input('Месяц: '))
+print(calendar.month(year, month))
+print('Всего хорошего!')
